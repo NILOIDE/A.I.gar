b83c0518ac67a32e5581ba65d89d75dabc31b47b
NILOIDE <nilstoltanso@gmail.com>
2019-03-20 01:05:11 +0100

    fixed test

--- src/test_thread_10.sh
+++ src/test_thread_10.sh
@@ -10,7 +10,7 @@ export OMP_NUM_THREADS=10
 module load matplotlib/2.1.2-foss-2018a-Python-3.6.4
 module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4
 module load h5py/2.7.1-foss-2018a-Python-3.6.4
-python3 -O ./aigar.py > out_thread.txt<<EOF
+python3 -O ./aigar.py > out_thread_10.txt<<EOF
 0
 0
 0
