6dce217fe3506e42059a1db464ae92740d6efe4f
NILOIDE <nilstoltanso@gmail.com>
2019-04-01 23:52:42 +0200

    Made tests

---
+++ src/test_thread_32.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --time=2-23:00:00
+#SBATCH --mem=20000
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nilstoltanso@gmail.com
+#SBATCH --cpus-per-task=4
+#SBATCH --output=test_32_%j.out
+export OMP_NUM_THREADS=1
+module load matplotlib/2.1.2-foss-2018a-Python-3.6.4
+module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4
+module load h5py/2.7.1-foss-2018a-Python-3.6.4
+python3 -O ./aigar.py > out_thread_32.txt<<EOF
+0
+0
+1
+EXP_REPLAY_ENABLED
+False
+1
+NUM_COLLECTORS
+32
+1
+MAX_TRAINING_STEPS
+100000
+0
+0
+EOF
