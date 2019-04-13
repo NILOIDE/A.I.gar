2878d8a414f85ebbe9cc69b4beced888820595d1
NILOIDE <nilstoltanso@gmail.com>
2019-04-13 16:23:35 +0200

    cnn test

--- src/test_thread.sh
+++ src/test_thread.sh
@@ -4,15 +4,20 @@
 #SBATCH --nodes=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nilstoltanso@gmail.com
-#SBATCH --cpus-per-task=4
-#SBATCH --output=ALGORITHM-"CACLA"_Default-"CACLA"_%j.out
+#SBATCH --cpus-per-task=2
+#SBATCH --output=cnn2.out
 export OMP_NUM_THREADS=1
 module load matplotlib/2.1.2-foss-2018a-Python-3.6.4
 module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4
 module load h5py/2.7.1-foss-2018a-Python-3.6.4
-python3 -O ./aigar.py > out_thread.txt<<EOF
+python3 -O ./aigar.py<<EOF
 0
-0
+1
+MAX_TRAINING_STEPS
+50000
+1
+CNN_REPR
+True
 0
 0
 EOF
--- src/test_thread_1.sh
+++ src/test_thread_1.sh
@@ -5,14 +5,19 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nilstoltanso@gmail.com
 #SBATCH --cpus-per-task=1
-#SBATCH --output=test_thread_1_%j.out
+#SBATCH --output=cnn1.out
 export OMP_NUM_THREADS=1
 module load matplotlib/2.1.2-foss-2018a-Python-3.6.4
 module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4
 module load h5py/2.7.1-foss-2018a-Python-3.6.4
-python3 -O ./aigar.py > out_thread.txt<<EOF
+python3 -O ./aigar.py<<EOF
 0
-0
+1
+MAX_TRAINING_STEPS
+50000
+1
+CNN_REPR
+True
 0
 0
 EOF
